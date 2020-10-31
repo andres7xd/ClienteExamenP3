8f10cf5b5d19f3d3604ef67c699b3c834bbe1ac7
luismc1615 <58350003+luismc1615@users.noreply.github.com>
2020-10-30 22:36:03 -0600

    Cambios ConnectionUtils

--- cliente_examen/src/main/java/org/una/cliente_examen/controllers/ControlTareasController.java
+++ cliente_examen/src/main/java/org/una/cliente_examen/controllers/ControlTareasController.java
@@ -9,6 +9,7 @@ import java.io.IOException;
 import java.net.URL;
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.ResourceBundle;
 import java.util.concurrent.ExecutionException;
@@ -22,11 +23,14 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
 import javafx.scene.control.TreeCell;
 import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeView;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
@@ -77,11 +81,32 @@ public class ControlTareasController implements Initializable {
 
     private Long capturarIdTarea;
 
+    TareasService tareasService = new TareasService();
+
+    private ImageView imgV;
+
+    Date date = new Date();
+    Date date2 = new Date();
+
+    private Image imgRojo;
+    private Image imgNaranja;
+    private Image imgAmarillo;
+    private Image imgVerde;
+
+    TextField txtProyectos = new TextField();
+    TextField txtTareas = new TextField();
+    TextField txtPadre = new TextField();
+    @FXML
+    private ComboBox<String> cbxFiltroColores;
+
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+
+        cbxFiltroColores.setValue("Todos");
+        cbxFiltroColores.getItems().addAll("Rojo", "Naranja", "Amarillo", "Verde", "Todos");
 
         LlenarTreeView();
         EventoTreeItem();
@@ -109,19 +134,39 @@ public class ControlTareasController implements Initializable {
             Logger.getLogger(ControlTareasController.class.getName()).log(Level.SEVERE, null, ex);
         }
 
+        imgRojo = new Image("org/una/cliente_examen/iconos/cuadro rojo.jpg");
+        imgNaranja = new Image("org/una/cliente_examen/iconos/cuadro naranja.jpg");
+        imgAmarillo = new Image("org/una/cliente_examen/iconos/cuadro amarillo.jpg");
+        imgVerde = new Image("org/una/cliente_examen/iconos/cuadro verde.jpg");
+
         TreeItem<String> root1 = new TreeItem<>("Proyectos");
         root1.setExpanded(false);
         for (int i = 0; i < proyectolist.size(); i++) {
             TreeItem<String> root = new TreeItem<>(proyectolist.get(i).getNombre());
             for (int j = 0; j < tareaslist.size(); j++) {
                 if (tareaslist.get(j).getProyectos().getId() == proyectolist.get(i).getId()) {
-                    TreeItem<String> item = new TreeItem<>(tareaslist.get(j).getDescripcion());
-                    root.getChildren().addAll(item);
+                    if ((tareaslist.get(j).getPorcentaje_avance() >= 0 && tareaslist.get(j).getPorcentaje_avance() <= 20 && cbxFiltroColores.getValue() == "Rojo") || (tareaslist.get(j).getPorcentaje_avance() >= 0 && tareaslist.get(j).getPorcentaje_avance() <= 20 && cbxFiltroColores.getValue() == "Todos")) {
+                        TreeItem<String> item = new TreeItem<>(tareaslist.get(j).getDescripcion(), new ImageView(imgRojo));
+                        root.getChildren().addAll(item);
+                    } else if ((tareaslist.get(j).getPorcentaje_avance() > 20 && tareaslist.get(j).getPorcentaje_avance() <= 50 && cbxFiltroColores.getValue() == "Naranja") || (tareaslist.get(j).getPorcentaje_avance() > 20 && tareaslist.get(j).getPorcentaje_avance() <= 50 && cbxFiltroColores.getValue() == "Todos")) {
+                        TreeItem<String> item = new TreeItem<>(tareaslist.get(j).getDescripcion(), new ImageView(imgNaranja));
+                        root.getChildren().addAll(item);
+                    } else if ((tareaslist.get(j).getPorcentaje_avance() > 50 && tareaslist.get(j).getPorcentaje_avance() <= 80 && cbxFiltroColores.getValue() == "Amarillo") || (tareaslist.get(j).getPorcentaje_avance() > 50 && tareaslist.get(j).getPorcentaje_avance() <= 80 && cbxFiltroColores.getValue() == "Todos")) {
+                        TreeItem<String> item = new TreeItem<>(tareaslist.get(j).getDescripcion(), new ImageView(imgAmarillo));
+                        root.getChildren().addAll(item);
+                    } else if ((tareaslist.get(j).getPorcentaje_avance() > 80 && tareaslist.get(j).getPorcentaje_avance() <= 100 && cbxFiltroColores.getValue() == "Verde") || (tareaslist.get(j).getPorcentaje_avance() > 80 && tareaslist.get(j).getPorcentaje_avance() <= 100 && cbxFiltroColores.getValue() == "Todos")) {
+                        TreeItem<String> item = new TreeItem<>(tareaslist.get(j).getDescripcion(), new ImageView(imgVerde));
+                        root.getChildren().addAll(item);
+                    }
                 }
             }
             root1.getChildren().add(root);
         }
         treeViewProyectos.setRoot(root1);
+    }
+
+    public void refrescarTreeView() {
+        LlenarTreeView();
     }
 
     public void EventoTreeItem() {
@@ -149,6 +194,9 @@ public class ControlTareasController implements Initializable {
     public void obtenerInfoTareas() {
         for (int i = 0; i < tareaslist.size(); i++) {
             if (tareaslist.get(i).getDescripcion().equalsIgnoreCase(compDescripcion)) {
+
+                tareasDTO = tareaslist.get(i);
+
                 txtId.setText(tareaslist.get(i).getId().toString());
                 LocalDate fechaInicio = parsearFecha(tareaslist.get(i).getFecha_inicio().toString());
                 dpFechaInicio.setValue(fechaInicio);
@@ -273,33 +321,41 @@ public class ControlTareasController implements Initializable {
     }
 
     @FXML
-    private void actionModificarTarea(ActionEvent event) throws IOException {
-        EventoTreeItem();
+    private void actionModificarTarea(ActionEvent event) throws IOException, InterruptedException, ExecutionException {
 
-        AppContext.getInstance().set("tareasDTO", tareasDTO);
-        AppContext.getInstance().set("ed", "edit");
+        date = java.sql.Date.valueOf(dpFechaInicio.getValue());
+        date2 = java.sql.Date.valueOf(dpFechaFinalizacion.getValue());
 
-        Parent root = FXMLLoader.load(App.class.getResource("CreacionTareas.fxml"));
-        Scene creacionDocs = new Scene(root);
-        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        window.setScene(creacionDocs);
-        window.show();
+        tareasDTO.setDescripcion(txtDescripcion.getText());
+        tareasDTO.setFecha_inicio(date);
+        tareasDTO.setFecha_finalizacion(date2);
+        tareasDTO.setImportancia(Double.parseDouble(txtImportancia.getText()));
+        tareasDTO.setPrioridad(Double.parseDouble(txtPrioridad.getText()));
+        tareasDTO.setUrgencia(Double.parseDouble(txtUrgencia.getText()));
+        tareasDTO.setPorcentaje_avance(Double.parseDouble(txtPorcentajeAvance.getText()));
+        tareasDTO.setProyectos(proyectosDTO);
+        tareasService.modify(tareasDTO.getId(), tareasDTO);
+
+//        Parent root = FXMLLoader.load(App.class.getResource("CreacionTareas.fxml"));
+//        Scene creacionDocs = new Scene(root);
+//        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+//        window.setScene(creacionDocs);
+//        window.show();
     }
 
     @FXML
     private void actionEliminarTarea(ActionEvent event) {
         TareasService tareasService = new TareasService();
-        System.out.println("aaaaaaaaaaaaaaaaaaaa" + capturarIdTarea);
         if (tareasDTO != null) {
             try {
                 tareasService.delete(capturarIdTarea);
-                Alert info = new Alert(Alert.AlertType.CONFIRMATION);
+                Alert info = new Alert(Alert.AlertType.INFORMATION);
                 info.setTitle("Mensaje");
                 info.setContentText("La tarea se eliminó correctamente");
                 info.showAndWait();
                 LlenarTreeView();
             } catch (Exception e) {
-                Alert info = new Alert(Alert.AlertType.CONFIRMATION);
+                Alert info = new Alert(Alert.AlertType.INFORMATION);
                 info.setTitle("Error");
                 info.setContentText("La tarea no se eliminó");
                 info.showAndWait();
@@ -310,5 +366,10 @@ public class ControlTareasController implements Initializable {
     @FXML
     private void actiontvProyectos(MouseEvent event) {
 
+    }
+
+    @FXML
+    private void ActionCbxFiltroColores(ActionEvent event) {
+        LlenarTreeView();
     }
 }
